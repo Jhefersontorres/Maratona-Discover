a4e95fb6d4d4aad8c839ef79bfdb1fc4111185cb
Jhefersontorres <jheferson_torres@hotmail.com>
2021-02-20 16:30:10 -0400

    Codigo Finalizado com Dark Mode inserido

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -214,3 +214,14 @@ const App = {
 }
 
 App.init()
+
+
+
+
+//Dark Mode
+const $html = document.querySelector("html")
+const $checkbox = document.querySelector("#switch")
+
+$checkbox.addEventListener('change', function(){
+    $html.classList.toggle('dark-mode')
+})
